d3692550ddc0ff999c42471d14bc39b1553161b8
Luis Chilin Capcha <luischilincapcha@gmail.com>
2023-12-17 19:29:47 -0500

    Actualizacion

---
+++ src/components/FormCrearTecnico.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import TextField from "@mui/material/TextField";
+
+const FormCrearTecnico = () => {
+  const [list, setList] = useState(JSON.parse(localStorage.getItem('tecnicos')) || [])
+
+
+  const [values, setValue] = useState({})
+
+  const handleChangue  = (event) => {
+    event.preventDefault()
+
+    const {name, value} = event.target;
+
+    setValue({...values, [name]: value} )
+
+  }
+
+  const handleSave = () => {    
+    list.push(values)
+    localStorage.setItem("tecnicos", JSON.stringify(list));
+    alert('Creado correctamente.')
+  }
+  
+
+  return (
+    
+    <div className="w-[350px] bg-white rounded-2xl grid place-items-center py-6 gap-4">
+    <h1>
+            CREAR TÉCNICO <span class="block"></span> DE ACUEROD A SU ESPECIALIDAD
+    </h1>
+
+      <TextField
+        name="name"
+        onChange={handleChangue}
+        label="Ingrese nombre:"
+        variant="standard"
+        client:only="react"
+      />
+
+      <TextField
+        name="years"
+        label="Ingrese años de experiencia:"
+        onChange={handleChangue}
+        variant="standard"
+        client:only="react"
+      />
+
+      <TextField
+        name="type"
+        label="Ingrese especialidad:"
+        onChange={handleChangue}
+        variant="standard"
+        client:only="react"
+      />
+
+      <TextField
+        name="img_path"
+        label="Ingrese img para su perfil:"
+        onChange={handleChangue}
+        variant="standard"
+        client:only="react"
+      />
+
+      <button
+        onClick={handleSave}
+        className="bg-gray-700 flex flex-row py-4 px-4 w-[200px] rounded-2xl text-white uppercase font-medium shadow-2xl hover:bg-slate-500 hover:text-white justify-center"
+      >Crear
+
+      </button>
+
+    </div>
+  )
+}
+
+export default FormCrearTecnico
